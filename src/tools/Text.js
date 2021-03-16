0d4654bcee59d0fe99a42133b9ef7725e64d9e29
RedFoxFinn <laborantti.antti@gmail.com>
2021-03-16 16:31:01 +0200

    fix: fixed issue with Text tool and unified text source uiTexts.json

--- src/tools/Text.js
+++ src/tools/Text.js
@@ -3,41 +3,41 @@ import uiTexts from '../settings/uiTexts.json';
 
 const header = (lang = 'en') => {
   switch (lang) {
-  case 'fi': return {text: uiTexts.navigation.header.fi, lang: lang};
-  case 'en': return {text: uiTexts.navigation.header.en, lang: lang};
-  default: return {text: uiTexts.navigation.header.en, lang: 'en'};
+  case 'fi': return {text: uiTexts.navigation.header.fi.toString(), lang: lang};
+  case 'en': return {text: uiTexts.navigation.header.en.toString(), lang: lang};
+  default: return {text: uiTexts.navigation.header.en.toString(), lang: 'en'};
   }
 };
 
 const footer = (lang = 'en') => {
   switch (lang) {
-  case 'fi': return {text: uiTexts.navigation.footer.fi, lang: lang};
-  case 'en': return {text: uiTexts.navigation.footer.en, lang: lang};
-  default: return {text: uiTexts.navigation.footer.en, lang: 'en'};
+  case 'fi': return {text: uiTexts.navigation.footer.fi.toString(), lang: lang};
+  case 'en': return {text: uiTexts.navigation.footer.en.toString(), lang: lang};
+  default: return {text: uiTexts.navigation.footer.en.toString(), lang: 'en'};
   }
 };
 
 const home = (lang = 'en') => {
   switch (lang) {
-  case 'fi': return {text: uiTexts.navigation.home.fi, lang: lang};
-  case 'en': return {text: uiTexts.navigation.home.en, lang: lang};
-  default: return {text: uiTexts.navigation.home.en, lang: 'en'};
+  case 'fi': return {text: uiTexts.navigation.home.fi.toString(), lang: lang};
+  case 'en': return {text: uiTexts.navigation.home.en.toString(), lang: lang};
+  default: return {text: uiTexts.navigation.home.en.toString(), lang: 'en'};
   }
 };
 
 const configure = (lang = 'en') => {
   switch (lang) {
-  case 'fi': return {text: uiTexts.navigation.configure.fi, lang: lang};
-  case 'en': return {text: uiTexts.navigation.configure.en, lang: lang};
-  default: return {text: uiTexts.navigation.configure.en, lang: 'en'};
+  case 'fi': return {text: uiTexts.navigation.configure.fi.toString(), lang: lang};
+  case 'en': return {text: uiTexts.navigation.configure.en.toString(), lang: lang};
+  default: return {text: uiTexts.navigation.configure.en.toString(), lang: 'en'};
   }
 };
 
 const stoptimes = (lang = 'en') => {
   switch (lang) {
-  case 'fi': return {text: uiTexts.navigation.stoptimes.fi, lang: lang};
-  case 'en': return {text: uiTexts.navigation.stoptimes.en, lang: lang};
-  default: return {text: uiTexts.navigation.stoptimes.en, lang: 'en'};
+  case 'fi': return {text: uiTexts.navigation.stoptimes.fi.toString(), lang: lang};
+  case 'en': return {text: uiTexts.navigation.stoptimes.en.toString(), lang: lang};
+  default: return {text: uiTexts.navigation.stoptimes.en.toString(), lang: 'en'};
   }
 };
 
